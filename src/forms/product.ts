7b190a324607a494b67826cc73d2c48c57e4b2d1
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2024-09-12 14:54:03 +0700

    pussh

---
+++ src/forms/product.ts
@@ -0,0 +1,23 @@
+export const productForm = [
+  {
+    key: "title",
+    value: "Products",
+    label: "Products name",
+    placeholder: "Enter product name",
+    type: "default",
+    required: true,
+    message: "Enter product name",
+    default_value: "",
+    displayLength: 400,
+  },
+  {
+    key: "price",
+    value: "Price",
+    label: "Price name",
+    type: "number",
+    placeholder: "",
+    message: "Enter price name",
+    default_value: "",
+    displayLength: 300,
+  },
+];
---
+++ src/models/ProductModel.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const productScheme = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: String,
+  description: String,
+  expiryDate: {
+    type: Date,
+  },
+  supplier: {
+    required: true,
+    type: String,
+  },
+  categories: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const ProductModel = mongoose.model("product", productScheme);
+export default ProductModel;
